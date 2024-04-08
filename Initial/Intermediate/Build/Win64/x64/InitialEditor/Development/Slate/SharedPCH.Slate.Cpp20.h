1ebf041cee873ba8d8e0c73076fc614412274a45
Frederik Gonge <45792897+V1kingGit@users.noreply.github.com>
2024-04-08 03:09:51 +0200

    Asset imports

---
+++ Initial/Intermediate/Build/Win64/x64/InitialEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Slate/Public/SlateSharedPCH.h
+#include "C:/Users/Frederik/Documents/GitHub/UnrealInitial/Initial/Intermediate/Build/Win64/x64/InitialEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
+#include "Runtime/Slate/Public/SlateSharedPCH.h"
---
+++ Initial/Intermediate/Build/Win64/x64/InitialEditor/Development/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
+#include "C:/Users/Frederik/Documents/GitHub/UnrealInitial/Initial/Intermediate/Build/Win64/x64/InitialEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.RTTI.Cpp20.h"
+#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
