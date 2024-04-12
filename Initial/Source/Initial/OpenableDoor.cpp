b139bfd513c38c4b1676632cf1b06919742b0d73
Frederik Gonge <45792897+V1kingGit@users.noreply.github.com>
2024-04-12 02:14:39 +0200

    Openable door

--- Initial/Source/Initial/OpenableDoor.cpp
+++ Initial/Source/Initial/OpenableDoor.cpp
@@ -55,10 +55,10 @@ void AOpenableDoor::Toggle()
 		if(Door != NULL)
 			GEngine->AddOnScreenDebugMessage(-1, 0.5f, FColor::Yellow, TEXT("Door not null"));
 	}
-	//if(IsOpen)
-	//	Door->SetActorRelativeRotation(FRotator(0.0f, 0.0f, -120.0f));
-	//else
-	//  Door->SetActorRelativeRotation(FRotator::ZeroRotator);
+	if(IsOpen)
+		Door->SetActorRelativeRotation(FRotator(0.0f, 230.0f, 0.0f));
+	else
+		Door->SetActorRelativeRotation(FRotator::ZeroRotator);
 }
 
 void AOpenableDoor::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
